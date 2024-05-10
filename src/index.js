383e81eefaab68aaffe461530d03b632cc3b3dd4
Alessandro1918 <alessandro1918@hotmail.com>
2024-05-10 09:44:50 -0300

    feat: create, copy to, read from, and remove temp dir inside server dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { Shazam } from "node-shazam"
-
+import * as fs from "fs"
 const app = express()
 const shazam = new Shazam()
 const PORT = process.env.PORT || 4000
@@ -10,9 +10,18 @@ app.get("/", (req, res) => {return res.send("Hello, world!")})
 
 //Recognise song based on audio file
 app.get("/audio/:id", async (req, res) => {
-  const path = `./audio_samples/${req.params.id}.m4a` //path = "./audio_samples/003.m4a"
-  shazam.recognise(path)
+
+  const srcPath = `./audio_samples/${req.params.id}.m4a` //path = "./audio_samples/003.m4a"
+  const tmpDir = "temp"
+  const filePath = `./${tmpDir}/${req.params.id}.m4a`
+
+  fs.mkdirSync(tmpDir, { recursive: true })
+
+  fs.copyFileSync(srcPath, filePath)
+
+  shazam.recognise(filePath)
     .then(response => {
+      fs.rmSync(tmpDir, { recursive: true, force: true })
       return res.json(response)
     })
 })
