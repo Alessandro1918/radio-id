6ddcedc6f611ef71612b6692dddcc2d23804dc40
Alessandro1918 <alessandro1918@hotmail.com>
2024-05-10 00:16:04 -0300

    feat: first express server route, reading local audio file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-console.log("Hello!")
+import express from "express"
+import { Shazam } from "node-shazam"
 
-import {Shazam} from "node-shazam"
+const app = express()
 const shazam = new Shazam()
+const PORT = process.env.PORT || 4000
 
-shazam.recognise("./audio_samples/003.m4a")
-  .then(response => {
-    console.log(response)
-  })
+//Test
+app.get("/", (req, res) => {return res.send("Hello, world!")})
+
+//Recognise song based on audio file
+app.get("/audio/:id", async (req, res) => {
+  const path = `./audio_samples/${req.params.id}.m4a` //path = "./audio_samples/003.m4a"
+  shazam.recognise(path)
+    .then(response => {
+      return res.json(response)
+    })
+})
+
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
