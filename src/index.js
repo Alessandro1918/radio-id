8baddd0988352798ca1dd52babaebf5234b92c17
Alessandro1918 <alessandro1918@hotmail.com>
2024-05-11 00:23:04 -0300

    feat: copy test audio file to vercel's '/tmp' dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { Shazam } from "node-shazam"
 import * as fs from "fs"
+import * as path from "path"
 
 const app = express()
 const shazam = new Shazam()
@@ -12,17 +13,16 @@ app.get("/", (req, res) => {return res.send("Hello, world!")})
 //Recognise song based on audio file
 app.get("/audio/:id", async (req, res) => {
 
-  const srcPath = `./audio_samples/${req.params.id}.m4a` //path = "./audio_samples/003.m4a"
-  const tmpDir = "temp"
-  const filePath = `./${tmpDir}/${req.params.id}.m4a`
+  const srcPath = path.join(process.cwd(), "audio_samples", `${req.params.id}.m4a`) //path = "./audio_samples/003.m4a"
+  const filePath = path.join("/tmp", `${req.params.id}.m4a`)
 
-  fs.mkdirSync(tmpDir, { recursive: true })
+  // fs.mkdirSync(tmpDir, { recursive: true })  //no need to init "/tmp" dir
 
   fs.copyFileSync(srcPath, filePath)
 
   shazam.recognise(filePath)
     .then(response => {
-      fs.rmSync(tmpDir, { recursive: true, force: true })
+      // fs.rmSync(tmpDir, { recursive: true, force: true })  //no need to remove "/tmp" dir
       return res.json(response)
     })
 })
