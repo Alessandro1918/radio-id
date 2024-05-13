0dce1f81aca58993ec3d973ed03833205207b4ea
Alessandro1918 <alessandro1918@hotmail.com>
2024-05-13 00:24:16 -0300

    feat: recording short audio file from stream's url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import { Shazam } from "node-shazam"
 import * as fs from "fs"
 import * as path from "path"
+import Recorder from "node-rtsp-recorder"
 
 const app = express()
 const shazam = new Shazam()
@@ -11,10 +12,10 @@ const PORT = process.env.PORT || 4000
 app.get("/", (req, res) => {return res.send("Hello, world!")})
 
 //Recognise song based on audio file
-app.get("/audio/:id", async (req, res) => {
+app.get("/audio/:filename", async (req, res) => {
 
-  const srcPath = path.join(process.cwd(), "audio_samples", `${req.params.id}.m4a`) //path = "./audio_samples/003.m4a"
-  const filePath = path.join("/tmp", `${req.params.id}.m4a`)
+  const srcPath = path.join(process.cwd(), "audio_samples", `${req.params.filename}`) //path = "./audio_samples/003.avi"
+  const filePath = path.join("/tmp", `${req.params.filename}`)
 
   // fs.mkdirSync(tmpDir, { recursive: true })  //no need to init "/tmp" dir
 
@@ -25,6 +26,24 @@ app.get("/audio/:id", async (req, res) => {
       // fs.rmSync(tmpDir, { recursive: true, force: true })  //no need to remove "/tmp" dir
       return res.json(response)
     })
+})
+
+//Record audio file from stream's URL
+app.get("/record/:streamUrl", (req, res) => {
+  var rec = new Recorder.Recorder({
+    url: req.params.streamUrl,  //stream = "https://cloud2.cdnseguro.com:20000/;" (Kiss FM)
+    timeLimit: 5, // time in seconds for each segmented audio file
+    folder: "/tmp/records",
+    type: "audio",
+  })
+  
+  rec.startRecording()
+
+  setTimeout(() => {
+      console.log("Stopping Recording")
+      rec.stopRecording()
+      rec = null
+  }, 5*1000)  //ms
 })
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
