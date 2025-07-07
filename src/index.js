16538edf5cf6e2e4e0cc4a1a9b13ad46e1eb407c
Alessandro1918 <alessandro1918@hotmail.com>
2025-07-07 17:44:24 -0300

    refactor: search radio with query params instead of streamURL @ api route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 //Step 4 — Processing Media With ffmpeg.wasm
 //https://www.digitalocean.com/community/tutorials/how-to-build-a-media-processing-api-in-node-js-with-express-and-ffmpeg-wasm
 
-//Temp dir notation:
-// "./tmp/records": localhost
-//  "/tmp/records": vercel
-//   "tmp/records": localhost
-
 //Requires:
 // require("dotenv").config()
 // const express = require("express")
@@ -15,6 +10,7 @@
 // const ffmpeg = require("fluent-ffmpeg")
 // const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg")
 // ffmpeg.setFfmpegPath(ffmpegInstaller.path)
+// const RadioBrowser = require('radio-browser')
 
 //Imports (package.json: "type": "module"):
 // import 'dotenv/config'
@@ -25,6 +21,7 @@ import { Shazam } from "node-shazam"
 import ffmpeg from "fluent-ffmpeg"       //V1 - https://www.npmjs.com/package/fluent-ffmpeg
 import ffmpegInstaller from "@ffmpeg-installer/ffmpeg"
 ffmpeg.setFfmpegPath(ffmpegInstaller.path)
+import RadioBrowser from "radio-browser"
 
 // console.log(ffmpegInstaller.path)
 // console.log(ffmpegInstaller.url)
@@ -34,66 +31,124 @@ const app = express()
 const shazam = new Shazam()
 const PORT = process.env.PORT || 4000
 
-//Uses "node-shazam" package to recognise data (artist, song title) from an audio file
-async function recognise(filepath) {
-  const response = await shazam.recognise(filepath)
-  
-  // return response
-  var data = {}
-  if (response != null) {
-    data["timestamp"] = response["timestamp"]
-    data["title"] = response["track"]["title"]
-    data["artist"] = response["track"]["subtitle"]
-    data["album"] = {}
-    data["album"]["cover"] = response["track"]["images"]["coverart"]
-    const metadtataAlbumTitle = response.track.sections[0].metadata.filter(e => e.title === "Album")
-    data["album"]["title"] = metadtataAlbumTitle.length > 0 ? metadtataAlbumTitle[0].text : "-"
-    const metadtataAlbumYear = response.track.sections[0].metadata.filter(e => e.title === "Released")
-    data["album"]["year"] = metadtataAlbumYear.length > 0 ? metadtataAlbumYear[0].text : "-"
+// Given a query for a radio ("name=kiss_fm" for "Kiss FM", for ex.), returns the radio info, like the stream URL ("https://cloud2.cdnseguro.com:20000/;", for ex.)
+async function search(query) {
+  try {
+    // API: https://api.radio-browser.info
+    // NPM wrapper: https://www.npmjs.com/package/radio-browser
+    // Example of working servers: https://fi1.api.radio-browser.info, https://de2.api.radio-browser.info
+    // Advanced search docs: https://de1.api.radio-browser.info/#Advanced_station_search 
+    // Advanced search example: http://de1.api.radio-browser.info/json/stations/search?name=kiss_FM&countrycode=BR
+    // query: "name=kiss_fm&countrycode=BR" => filters: { name: 'kiss_fm', countrycode: 'BR' }
+    const filters = {}
+    query.split("&").map(param => {
+      filters[param.split("=")[0]] = param.split("=")[1]
+    })
+    const data = await RadioBrowser.searchStations(filters)
+    const radio = {
+      name: data[0].name,
+      state: data[0].state,
+      countrycode: data[0].countrycode,
+      stream: data[0].url,
+      site: data[0].homepage,
+      icon: data[0].favicon
+    }
+    console.log(`Radio 1/${data.length}: ${radio.name} (${radio.state}-${radio.countrycode})`) // Radio 1/3: Rádio Kiss FM - 92.5 (São Paulo-BR)
+    return radio
+  } catch (err) {
+    console.log("Search error: Could not find the radio")
+    throw new Error("Error: Could not find the radio")
   }
-  return data
 }
 
-//Test
-app.get("/", (req, res) => {return res.send("Hello, world!")})
+// Given a URL stream, records a few seconds of it in a local audio file 
+function record(streamURL) {
 
-//Record audio file from stream's URL, and try to recognise the music playing on it
-app.get("/recognise/:stream", (req, res) => {
-
+  //Temp dir notation:
+  // "./tmp/records": localhost
+  //  "/tmp/records": vercel
+  //   "tmp/records": localhost
   const pathRecord = "./tmp/record.m4a"
 
-  ffmpeg()
-    .input(req.params.stream)         //stream = "https://cloud2.cdnseguro.com:20000/;" (Kiss FM)
-    // .input("./tmp/records/2024-05-25/audio/2024-05-25-16-55-51.avi")
-    
-    .duration(5)
+  return new Promise((resolve, reject) => {
 
-    .save(pathRecord)
+    ffmpeg()
+      .input(streamURL)  
+      // .input("https://cloud2.cdnseguro.com:20000/;") // Kiss FM
+      // .input("./tmp/records/2024-05-25/audio/2024-05-25-16-55-51.avi") // Pretenders - Back on the Chain Gang
+      
+      .duration(5)
 
-    // .output("test.mp3")
-    // .run()
+      .save(pathRecord)
 
-    // .outputOptions("-t 5")
-    // .save("test.m4a")
+      // .output("test.mp3")
+      // .run()
 
-    .on("start", function(command) {console.log("Spawned Ffmpeg with command: " + command)})
-    // .on("progress", function (progress) {console.log("Processing: " + progress.percent + "% done")})
-    .on("error", function (err) {
-      console.log("An error occurred: " + err.message)
-      return res.status(500).send("An error has occurred during recognition; please try again")
-    })
-    .on("end", async function () {
-      console.log("Processing finished!")
+      // .outputOptions("-t 5")
+      // .save("test.m4a")
 
-      const data = await recognise(pathRecord)
-      if (Object.keys(data).length == 0) {
-        console.log("Data not found :(")
-        return res.status(400).send("Data not found")
-      } else {
-        console.log("Data found!")
-        return res.status(200).json(data)
-      }
-    })
+      .on("start", function(command) {console.log("Spawned Ffmpeg with command: " + command)})
+      .on("progress", function (progress) {console.log(`Processing: ${progress.timemark.split(":")[2].split(".")[0]}.${String(progress.timemark.split(":")[2].split(".")[1]).padEnd(3, "0")}s`)}) //00:00:04.50 -> 4.500s
+      .on("error", function (err) {
+        console.log("Ffmpeg error: " + err.message)
+        reject(new Error(`Ffmpeg error: ${err.message}`))
+        throw new Error(`Error: ${err.message}`)
+      })
+      .on("end", async function () {
+        console.log("Processing finished!")
+        resolve(pathRecord)
+      })
+  })
+}
+
+//Uses "node-shazam" package to recognise data (artist, song title) from an audio file
+async function recognize(filepath) {
+  const response = await shazam.recognise(filepath)
+  
+  try {
+    const data = {}
+    data["timestamp"] = response["timestamp"]
+    data["track"] = {}
+    data["track"]["title"] = response["track"]["title"]
+    data["track"]["artist"] = response["track"]["subtitle"]
+    data["track"]["album"] = {}
+    data["track"]["album"]["cover"] = response["track"]["images"]["coverart"]
+    const metadtataAlbumTitle = response.track.sections[0].metadata.filter(e => e.title === "Album")
+    data["track"]["album"]["title"] = metadtataAlbumTitle.length > 0 ? metadtataAlbumTitle[0].text : "-"
+    const metadtataAlbumYear = response.track.sections[0].metadata.filter(e => e.title === "Released")
+    data["track"]["album"]["year"] = metadtataAlbumYear.length > 0 ? metadtataAlbumYear[0].text : "-"
+    console.log("Music data found!")
+    return data
+  } catch (err) {
+    console.log(`Shazam error: ${err}`)
+    throw new Error("Error: Data not found :(")
+  }
+}
+
+// Test
+app.get("/", (req, res) => {return res.send("Hello, world!")})
+
+// Record audio file from stream's URL, and try to recognise the music playing on it
+// Usage: http://localhost:4000/api/v1/id/name=kiss_fm&countrycode=BR
+app.get("/api/v1/id/:query", async (req, res) => {
+  try {
+    const query = req.params.query
+
+    const radio = await search(query)
+
+    const pathRecord = await record(radio.stream)
+
+    const data = await recognize(pathRecord)
+
+    data["radio"] = radio
+
+    return res.status(200).json(data)
+  } catch (err) {
+    // return res.status(500).send(err.message)
+    if (String(err.message).includes("radio")) { return res.status(404).json({"message": err.message}) } // search error
+    if (String(err.message).includes("data")) { return res.status(400).json({"message": err.message}) }  // shazam error
+    return res.status(500).json({"message": err.message})  // ffmpeg error
+  }
 })
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
